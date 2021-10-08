ff2686d038b9a9d1f5ac2a29100226a1fb1a4fbc
AurélienMottier <amottier@palo-it.com>
2021-10-08 19:23:09 +0800

    [Cloud Stream] Initial commmit.

---
+++ cloud-stream/cloud-stream-api-download/src/main/java/aurelienmottier/cloud/stream/Start.java
@@ -0,0 +1,14 @@
+package aurelienmottier.cloud.stream;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) {
+        run(Start.class, args);
+    }
+
+}
---
+++ cloud-stream/cloud-stream-api-stream/src/main/java/aurelienmottier/cloud/stream/controller/StreamController.java
@@ -0,0 +1,19 @@
+package aurelienmottier.cloud.stream.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("stream")
+public class StreamController {
+
+    @GetMapping
+    public ResponseEntity<String> dummy() {
+        return new ResponseEntity<>("It's working!", OK);
+    }
+
+}
