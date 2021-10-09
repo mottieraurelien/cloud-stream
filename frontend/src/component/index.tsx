7ad38de45a53ae23add783b0b26ec53d815ffe36
AurélienMottier <amottier@palo-it.com>
2021-10-09 11:03:08 +0800

    [Cloud Stream] Frontend with React : initial commit.

---
+++ frontend/src/component/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CloudStream() {
+    return <h1>YO !</h1>
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CloudStream from "./component";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <CloudStream/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
